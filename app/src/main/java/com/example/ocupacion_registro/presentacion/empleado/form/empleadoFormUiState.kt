1bfb2b7130b541b77fd4f92384e29ad85d730c2b
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-05-16 18:02:31 -0400

    Feat:crea empleadoFormUiState.kt

---
+++ app/src/main/java/com/example/ocupacion_registro/presentacion/empleado/form/empleadoFormUiState.kt
@@ -0,0 +1,16 @@
+package com.example.ocupacion_registro.presentacion.empleado.form
+
+data class empleadoFormUiState(
+
+ val empleadoId:Int?= null,
+ val nombres : String?="",
+ val sexo: Char?=null,
+ val fechaIngreso: String?="",
+ val sueldo: Double?=null,
+ val isSaved: Boolean=false,
+ val isDeleting: Boolean=false,
+ val isNew: Boolean=true,
+ val saved: Boolean=false,
+ val deleted: Boolean=false
+
+)
