f7af6bf5d5b8c9c48e637dffdcd834d43c43d319
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-05-16 18:38:46 -0400

    Feat:Agrega empleadoRepositoryImp.kt

--- app/src/main/java/com/example/ocupacion_registro/data/empleado/local/repository/empleadoRepositoryImp.kt
+++ app/src/main/java/com/example/ocupacion_registro/data/empleado/local/repository/empleadoRepositoryImp.kt
@@ -1,5 +1,6 @@
 package com.example.ocupacion_registro.data.empleado.local.repository
 
+import androidx.constraintlayout.helper.widget.Flow
 import com.example.ocupacion_registro.data.empleado.local.empleadoDao
 import com.example.ocupacion_registro.domain.empleado.repository.empleadoRepository
 import javax.inject.Inject
@@ -7,5 +8,28 @@ import javax.inject.Inject
 class empleadoRepositoryImp  @Inject constructor(
     private val localDataSource: empleadoDao
 ) : empleadoRepository {
+    override fun observeAllEmpl(): Flow<List<Empleado>> {
+        return localDataSource.observeAllEmpl().map { entities ->
+            entities.map { it.toDomain() }
+        }
+    }
+
+    override suspend fun getByIdEmpl(id: Int): Empleado? {
+        return localDataSource.getByIdEmpl(id)?.toDomain()
+    }
+
+    override suspend fun upsertEmpl(empleado: Empleado): Int {
+        localDataSource.upsert(empleado.toEntity())
+        return empleado.empleadoId ?: 0
+    }
+
+    override suspend fun eliminarByIdEmpl(id: Int) {
+        localDataSource.deleteIdEmpl(id)
+    }
+
+    override suspend fun eliminarEmpl(empleado: Empleado) {
+        localDataSource.deleteEmpl(empleado.toEntity())
+    }
+
 
 }
