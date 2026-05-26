36e4d6bc4b2c25603c8b79b14129e526d841b085
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-05-26 15:15:57 -0400

    feat: crea modelo de registro horas en domain

---
+++ app/src/main/java/com/example/ocupacion_registro/domain/registroHoras/model/registroHorasEmpleado.kt
@@ -0,0 +1,11 @@
+package com.example.ocupacion_registro.domain.registroHoras.model
+
+data class registroHorasEmpleado (
+
+    val registroId: Int,
+    val empleadoId: Int,
+    val horasExtra: Double,
+    val horasNocturnas: Double,
+
+
+)
