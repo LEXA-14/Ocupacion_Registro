4adddef75d9b74979c64070aade619d7c3199536
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-05-25 20:31:55 -0400

    feat: Actualiza base de datos para incluir entidad de registro de horas

--- app/src/main/java/com/example/ocupacion_registro/data/ocupacion/local/database.kt
+++ app/src/main/java/com/example/ocupacion_registro/data/ocupacion/local/database.kt
@@ -4,14 +4,18 @@ import androidx.room.RoomDatabase
 import com.example.ocupacion_registro.data.empleado.local.empleadoDao
 import com.example.ocupacion_registro.data.empleado.local.empleadoEntity
 import com.example.ocupacion_registro.data.ocupacion.local.ocupacionDao
+import com.example.ocupacion_registro.data.registroHorasEmpleado.registroHorasEmpleadoDao
+import com.example.ocupacion_registro.data.registroHorasEmpleado.registroHorasEmpleadoEntity
 
 @Database(
     entities = [ocupacionEntity::class,
-    empleadoEntity::class],
-    version=4
+    empleadoEntity::class,
+        registroHorasEmpleadoEntity::class],
+    version=5
 )
 
 abstract class ocupacionDatabase:RoomDatabase(){
     abstract fun ocupacionDao(): ocupacionDao
     abstract fun empleadoDao(): empleadoDao
+    abstract fun registroHorasEmpleadoDao(): registroHorasEmpleadoDao
 }
