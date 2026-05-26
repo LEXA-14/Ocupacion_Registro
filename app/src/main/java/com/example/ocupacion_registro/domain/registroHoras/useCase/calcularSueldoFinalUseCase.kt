cca5650491c69aab67dcdc7b3de8bac26d5839f6
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-05-26 17:20:08 -0400

    feat: implementa use case que calcula el sueldo final

---
+++ app/src/main/java/com/example/ocupacion_registro/domain/registroHoras/useCase/calcularSueldoFinalUseCase.kt
@@ -0,0 +1,54 @@
+package com.example.ocupacion_registro.domain.registroHoras.useCase
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.example.ocupacion_registro.domain.empleado.repository.empleadoRepository
+import com.example.ocupacion_registro.domain.registroHoras.model.registroHorasEmpleado
+import com.example.ocupacion_registro.domain.registroHoras.repository.registroHorasRepository
+import com.example.ocupacion_registro.domain.registroHoras.validaciones.validateEmpleadoExiste
+import com.example.ocupacion_registro.domain.registroHoras.validaciones.validateFecha
+import com.example.ocupacion_registro.domain.registroHoras.validaciones.validateHorasExtras
+import com.example.ocupacion_registro.domain.registroHoras.validaciones.validateHorasNocturnas
+import javax.inject.Inject
+import kotlin.math.min
+
+class CalcularSueldoFinalUseCase @Inject constructor(
+    private val empleadoRepository: empleadoRepository,
+    private val registroHorasRepository: registroHorasRepository
+) {
+    @RequiresApi(Build.VERSION_CODES.O)
+    suspend operator fun invoke(
+        horas: registroHorasEmpleado
+    ): Result<Double> {
+        val empleado = empleadoRepository.getByIdEmpl(horas.empleadoId)
+
+
+        validateEmpleadoExiste(empleado).let { if (!it.isValid) return Result.failure(Exception(it.error)) }
+        validateHorasExtras(horas.horasExtras).let { if (!it.isValid) return Result.failure(Exception(it.error)) }
+        validateHorasNocturnas(horas.horasNocturnas).let { if (!it.isValid) return Result.failure(Exception(it.error)) }
+        validateFecha(horas.fecha).let { if (!it.isValid) return Result.failure(Exception(it.error)) }
+
+
+        val valorHora    = (empleado!!.sueldo /23.3)/8
+        val horasMax35=min(horas.horasExtras,28.0)
+        var pagoExtra = horasMax35 * (valorHora* 1.35)
+
+
+        if(horas.horasExtras > 28){
+            var horas100=horas.horasExtras-28
+            pagoExtra +=horas100 * (valorHora * 2)
+        }
+        if(horas.horasNocturnas>0){
+            pagoExtra +=horas.horasNocturnas *(valorHora*0.15)
+        }
+
+        val sueldoFinal=empleado.sueldo + pagoExtra
+        registroHorasRepository.upsertHoras(horas)
+
+        empleadoRepository.upsertEmpl(
+            empleado.copy(sueldoFinal = sueldoFinal)
+        )
+
+        return Result.success(sueldoFinal)
+    }
+}
