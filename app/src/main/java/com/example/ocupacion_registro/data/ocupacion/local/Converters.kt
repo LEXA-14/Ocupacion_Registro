fc44824696ddfa0b81421bcb5ecf5c9c1e690163
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-05-28 14:31:49 -0400

    feat: agrega converts a database.kt

---
+++ app/src/main/java/com/example/ocupacion_registro/data/ocupacion/local/Converters.kt
@@ -0,0 +1,16 @@
+package com.example.ocupacion_registro.data.ocupacion.local
+
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.room.TypeConverter
+import java.time.LocalDate
+
+class Converters {
+    @TypeConverter
+    fun fromLocalDate(date: LocalDate): String = date.toString()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @TypeConverter
+    fun toLocalDate(value: String): LocalDate = LocalDate.parse(value)
+}
--- app/src/main/java/com/example/ocupacion_registro/data/ocupacion/local/database.kt
+++ app/src/main/java/com/example/ocupacion_registro/data/ocupacion/local/database.kt
@@ -1,17 +1,23 @@
 package com.example.ocupacion_registro.data.ocupacion.local
+
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.ocupacion_registro.data.empleado.local.empleadoDao
 import com.example.ocupacion_registro.data.empleado.local.empleadoEntity
+import com.example.ocupacion_registro.data.ocupacion.local.Converters
 import com.example.ocupacion_registro.data.ocupacion.local.ocupacionDao
 import com.example.ocupacion_registro.data.registroHorasEmpleado.registroHorasEmpleadoDao
 import com.example.ocupacion_registro.data.registroHorasEmpleado.registroHorasEmpleadoEntity
+
+@TypeConverters(Converters::class)
 
 @Database(
     entities = [ocupacionEntity::class,
     empleadoEntity::class,
         registroHorasEmpleadoEntity::class],
-    version=5
+    version=8,
+    exportSchema = false
 )
 
 abstract class ocupacionDatabase:RoomDatabase(){
