704d0b1b6faadaf08e71e45b6aa3b0815270412e
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-05-28 14:46:59 -0400

    feat: crea registroHorasListaUIState.kt

---
+++ app/src/main/java/com/example/ocupacion_registro/presentacion/registroHorasEmpleados/lista/registroHorasListaUIState.kt
@@ -0,0 +1,11 @@
+package com.example.ocupacion_registro.presentacion.registroHorasEmpleados.lista
+
+import com.example.ocupacion_registro.domain.registroHoras.model.registroHorasEmpleado
+
+class registroHorasListaUIState (
+
+    val isLoading: Boolean = false,
+    val registros: List<registroHorasEmpleado> = emptyList(),
+    val message: String? = null,
+    val error: String? = null
+    )
