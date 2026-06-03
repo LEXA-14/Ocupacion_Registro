1e77f9ea607fe67d0063ae7075f05dcb42b052f3
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-06-03 15:50:25 -0400

    Feat: Mide el size de la ventana actual

--- app/src/main/java/com/example/ocupacion_registro/MainActivity.kt
+++ app/src/main/java/com/example/ocupacion_registro/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,11 +19,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val windowSizeClass=calculateWindowSizeClass(this)
+            val widthSizeClass=windowSizeClass.widthSizeClass
+
             Ocupacion_RegistroTheme {
+
                ocupacionNavHost()
                 }
             }
